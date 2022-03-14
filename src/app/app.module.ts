d36a720f6695dda1d88b86590277187a74fb7f25
liudmila <gonchikova94@gmail.com>
2022-03-14 23:35:31 +0800

    add libraries and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBigComponent } from './components/menu-big/menu-big.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBigComponent
   ],
   imports: [
     BrowserModule,
